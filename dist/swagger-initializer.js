bdddf4716a9ab4587c7ffb3ca82eb44d15aee0eb
Srishti Jain <100708010+srishti-234@users.noreply.github.com>
2022-08-22 12:58:51 +0530

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -9,7 +9,7 @@ window.onload = function() {
     requestInterceptor:
        (request) => {
        console.log("request.credentials 1", request.credentials)
-       request.credentials = 'include'
+       request.credentials = true
        console.log("request.credentials 2", request.credentials)
        return request;
     },
